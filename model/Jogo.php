bb25981fc02dbf393ed02ac6a216005cb5cc5d78
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-08 16:46:54 -0300

    Update Jogo.php

--- model/Jogo.php
+++ model/Jogo.php
@@ -1,120 +1,78 @@
 <?php
-//Modelo para Aluno
-
-include_once(__DIR__ . "/Categoria.php");
-
+// Modelo para Jogo
 class Jogo {
 
-    private ?int $id;
-    private ?string $nome; 
-    private ?int $empresa;
-    private ?string $categoria;
+    private $id;
+    private $nome;
+    private $empresa;
+    private $categoria;
+    private $anoLancamento;
+    private $descricao;
+    private $preco;
 
 
-    //Construtor da classe
     public function __construct() {
         $this->id = 0;
-        $this->categoria = null;        
+        $this->categoria = null;
+        $this->preco = 0.0;
+
     }
 
-    /**
-     * Get the value of id
-     */ 
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
-
-        return $this;
     }
 
-    /**
-     * Get the value of nome
-     */ 
-    public function getNome()
-    {
+    public function getNome() {
         return $this->nome;
     }
 
-    /**
-     * Set the value of nome
-     *
-     * @return  self
-     */ 
-    public function setNome($nome)
-    {
+    public function setNome($nome) {
         $this->nome = $nome;
-
-        return $this;
     }
 
-    /**
-     * Get the value of idade
-     */ 
-    public function getEmpresa()
-    {
+    public function getEmpresa() {
         return $this->empresa;
     }
 
-    /**
-     * Set the value of idade
-     *
-     * @return  self
-     */ 
-    public function setEmpresa($empresa)
-    {
+    public function setEmpresa($empresa) {
         $this->empresa = $empresa;
-
-        return $this;
     }
 
-    /**
-     * Get the value of categoria
-     */ 
-    public function getCategoria()
-    {
+
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    public function getCategoria() {
         return $this->categoria;
     }
 
-    public function getECategoriaDesc() {
-        switch($this->categoria) {
-            case 'A':
-                return "Ação";
-            case 'R':
-                return "RPG";
-            case 'E':
-                return "Esporte";
-            case 'S':
-                return "Simulação";
-            case 'V':
-                return "Aventura";
-            case 'P':
-                return "Puzzle";
-            default:
-                return "";
-        }
-    }
-    
-
-    /**
-     * Set the value of categoria
-     *
-     * @return  self
-     */ 
-    public function setCategoria($categoria)
-    {
-        $this->categoria = $categoria;
-
-        return $this;
+    public function getAnoLancamento() {
+        return $this->anoLancamento;
     }
 
+    public function setAnoLancamento($anoLancamento) {
+        $this->anoLancamento = $anoLancamento;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
 }
+?>
