e0df7c5ee9f1727f6cab7de2ba739f36744a789b
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-08 17:01:46 -0300

    Update jogos.sql

--- jogos.sql
+++ jogos.sql
@@ -1,9 +1,10 @@
-
-CREATE TABLE jogos ( 
+CREATE TABLE jogo ( 
   id int AUTO_INCREMENT NOT NULL, 
   nome varchar(70) NOT NULL,
   empresa varchar(70) NOT NULL,
-  categorias varchar(20) NOT NULL,
+  categoria varchar(20) NOT NULL,
+  anoLancamento varchar(20) NOT NULL,
+  descricao varchar(20) NOT NULL,
+  preco DECIMAL(10,2) NOT NULL,
   CONSTRAINT pk_jogos PRIMARY KEY (id) 
 );
-
