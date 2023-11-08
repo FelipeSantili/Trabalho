d354a4b703b9d739becc685da693fd4b9710862a
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-08 16:49:41 -0300

    Update JogoService.php

--- service/JogoService.php
+++ service/JogoService.php
@@ -1,38 +1,75 @@
 <?php
-
 include_once(__DIR__ . "/../model/Jogo.php");
 include_once(__DIR__ . "/../dao/JogoDAO.php");
 
 class JogoService {
-
     private JogoDAO $jogoDAO;
 
     public function __construct() {
-        $this->jogoDAO = new JogoDAO();        
+        $this->jogoDAO = new JogoDAO();
     }
 
     public function validarDados(Jogo $jogo) {
         $erros = array();
 
-        //Validação do campo nome
-        if(! $jogo->getNome())
-            array_push($erros, "Informe o nome!");
-        else {
-            //Validar se um jogo com o mesmo nome já foi cadastrado
-            $jogoMesmoNome = $this->jogoDAO->findByNome($jogo->getNome());
-            if($jogoMesmoNome && $jogoMesmoNome->getId() != $jogo->getId())
-                array_push($erros, "Um jogo com este nome já foi cadastrado!");
+        if (empty($jogo->getNome())) {
+            array_push($erros, "O campo Nome é obrigatório.");
         }
 
-       
-        if(! $jogo->getEmpresa())
-            array_push($erros, "Informe o nome da empresa!");
+        if (empty($jogo->getEmpresa())) {
+            array_push($erros, "O campo Empresa é obrigatório.");
+        }
 
-        
-        if(! $jogo->getCategoria())
-            array_push($erros, "Informe a categoria!");
+        if (empty($jogo->getCategoria())) {
+            array_push($erros, "O campo Categoria é obrigatório.");
+        }
+
+        if (empty($jogo->getAnoLancamento())) {
+            array_push($erros, "O campo Ano de Lançamento é obrigatório.");
+        }
+
+        if (empty($jogo->getDescricao())) {
+            array_push($erros, "O campo Descrição é obrigatório.");
+        }
+
+        if (empty($jogo->getPreco())) {
+            array_push($erros, "O campo Preço é obrigatório.");
+        }
+
+
 
         return $erros;
     }
 
+    public function listarJogos() {
+        return $this->jogoDAO->list();
+    }
+
+    public function inserirJogo(Jogo $jogo) {
+        $erros = $this->validarDados($jogo);
+
+        if (empty($erros)) {
+            $this->jogoDAO->insert($jogo);
+        }
+
+        return $erros;
+    }
+
+    public function atualizarJogo(Jogo $jogo) {
+        $erros = $this->validarDados($jogo);
+
+        if (empty($erros)) {
+            $this->jogoDAO->update($jogo);
+        }
+
+        return $erros;
+    }
+
+    public function buscarJogoPorId($id) {
+        return $this->jogoDAO->findById($id);
+    }
+
+    public function excluirJogo($id) {
+        $this->jogoDAO->deleteById($id);
+    }
 }
