f39dc9bed17af28a6c7f7743dda1843cc590626a
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-08 17:00:51 -0300

    Delete view/login directory

--- view/login/sair.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//Pagina para sair da aplicação (logoff do usuário)
-
-include_once(__DIR__ . "/../../controller/LoginController.php");
-
-$loginCont = new LoginController();
-$loginCont->deslogar();
-header("location: " . BASE_URL . "/view/login/login.php");
