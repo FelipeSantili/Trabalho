08e8011d007ef529738cf936c6c95e407a093a06
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-08 16:58:26 -0300

    Update menu.php

--- view/include/menu.php
+++ view/include/menu.php
@@ -1,33 +1,21 @@
 <?php
-include_once(__DIR__ . "/../../util/config.php");
-
-$nome = "[NOME]";
+    include_once(__DIR__ . "/../../util/config.php");
 ?>
-<nav class="navbar navbar-expand-md navbar-light bg-info">
-    <a class="navbar-brand" href="#">CRUD Alunos</a>
 
-    <button class="navbar-toggler" type="button" 
-        data-toggle="collapse" data-target="#navSite">
+<nav class="navbar navbar-expand-md navbar-light bg-info">
+    <a class="navbar-brand" href="<?= BASE_URL ?>">CRUD Jogos</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navSite">
         <span class="navbar-toggler-icon"></span>
-    </button>
+    </button>   
 
     <div class="collapse navbar-collapse" id="navSite">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_URL ?>">Home</a>
+                <a class="nav-link" href="<?= BASE_URL . "/view/jogos/listar.php" ?>">Listar Jogos</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navDropDown" data-toggle="dropdown">Cadastros</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="<?= BASE_URL . "/view/alunos/listar.php" ?>">Alunos</a>
-                    <a class="dropdown-item" href="#">Turmas</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navDropDown" data-toggle="dropdown"><?= $nome ?></a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="<?= BASE_URL . "/view/login/sair.php" ?>">Sair</a>
-                </div>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL . "/view/jogos/inserir.php" ?>">Inserir Jogo</a>
             </li>
         </ul>
     </div>
