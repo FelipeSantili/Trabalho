45ae12f29e7bf739ccb45f40a86aee6b7ce1206c
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-16 04:06:37 -0300

    Update menu.php

--- view/include/menu.php
+++ view/include/menu.php
@@ -2,20 +2,23 @@
     include_once(__DIR__ . "/../../util/config.php");
 ?>
 
-<nav class="navbar navbar-expand-md navbar-light bg-info">
-    <a class="navbar-brand" href="<?= BASE_URL ?>">CRUD Jogos</a>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Jogos</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navSite">
         <span class="navbar-toggler-icon"></span>
-    </button>   
+    </button>
 
     <div class="collapse navbar-collapse" id="navSite">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_URL . "/view/jogos/listar.php" ?>">Listar Jogos</a>
+                <a class="nav-link" href="<?= BASE_URL ?>">Início</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_URL . "/view/jogos/inserir.php" ?>">Inserir Jogo</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navDropDown" data-toggle="dropdown">Cadastros</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="<?= BASE_URL . "/view/jogos/listar.php"?>">Jogos</a>
+                </div>
             </li>
         </ul>
     </div>
