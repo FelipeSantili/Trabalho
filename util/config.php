80497fc3f328b55df2e80d608f80d9addeb304a1
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-16 04:05:00 -0300

    Update config.php

--- util/config.php
+++ util/config.php
@@ -1,16 +1,8 @@
 <?php
 
-/* Ativar estas configurações caso os erros não
-estajam sendo exibidos no navegador */
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
-
-
-//Configurar essas variáveis de acordo com o seu ambiente
 define("DB_HOST", "localhost");
-define("DB_NAME", "jogo");
+define("DB_NAME", "jogos_crud");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 
-//Configurar variável do diretório base da aplicação
-define("BASE_URL", "/web/trabalho");
+define("BASE_URL", "/web/trabalho2");
