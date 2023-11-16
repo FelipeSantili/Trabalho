643f8057c06d3bf79aafafd9c7c32134c5fa312d
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-16 04:03:16 -0300

    Create Plataforma.php

---
+++ model/Plataforma.php
@@ -0,0 +1,31 @@
+<?php
+
+class Plataforma {
+    private ?int $id;
+    private ?string $nome;
+
+    public function __toString() {
+        return $this->nome;
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome(): ?string {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+}
