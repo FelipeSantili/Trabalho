15a08d1db3aaf5bc7fb2567fffdfcd0e536c8b71
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-16 04:06:03 -0300

    Update header.php

--- view/include/header.php
+++ view/include/header.php
@@ -4,35 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>CRUD Jogos</title>
-    <style>
-      html, body {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      }
-      #wrapper{
-      min-height: 100%;
-      position: relative;
-      }
-      div.body-content{
-        
-      padding-bottom: 100px;
-      }
-      footer{
-      background: #ffab62;
-      width: 100%;
-      height: 100px;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      }
-    </style>
+    <title>Jogos</title>
   </head>
   <body>
+    <div class="container"> <!-- index -->
+
     <?php include_once(__DIR__ . "/../../view/include/menu.php") ?>
-    <div class="container">
-      
