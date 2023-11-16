a51f93088e4e46ccf8a13dfd05f3d1a507a3e941
Luis Felipe Santili <felipesantili@gmail.com>
2023-11-16 04:08:33 -0300

    Update inserir.php

--- view/jogos/inserir.php
+++ view/jogos/inserir.php
@@ -1,38 +1,47 @@
 <?php
+
 include_once(__DIR__ . "/../../model/Jogo.php");
+include_once(__DIR__ . "/../../model/Categoria.php");
+include_once(__DIR__ . "/../../model/Plataforma.php");
 include_once(__DIR__ . "/../../controller/JogoController.php");
 
 $msgErros = "";
 $jogo = null;
 
-if (isset($_POST['submetido'])) {
-   
-    $nome = trim($_POST['nome']);
-    $empresa = trim($_POST['empresa']);
-    $categoria = trim($_POST['categoria']);
-    $anoLancamento = is_numeric($_POST['anoLancamento']) ? $_POST['anoLancamento'] : null;
-    $descricao = trim($_POST['descricao']);
-    $preco = is_numeric($_POST['preco']) ? $_POST['preco'] : null; 
-
+if(isset($_POST['submetido'])){
     
-    $jogo = new Jogo();
-    $jogo->setNome($nome);
-    $jogo->setEmpresa($empresa);
-    $jogo->setCategoria($categoria);
-    $jogo->setAnoLancamento($anoLancamento);
-    $jogo->setDescricao($descricao);
-    $jogo->setPreco($preco);
+    $nome = trim($_POST['nome']);
+    $anoLancamento = is_numeric($_POST['anoLancamento']) ? $_POST['anoLancamento'] : NULL;
+    $empresa = trim($_POST['empresa']);
+    $preco = is_numeric($_POST['preco']) ? $_POST['preco']: null;
+    $idCategoria = is_numeric($_POST['categoria']) ? $_POST['categoria'] : NULL;
+    $idPlataforma = is_numeric($_POST['plataforma']) ? $_POST['plataforma'] : NULL;
 
-   
-    $jogoCont = new JogoController();
+    $jogo = new jogo();
+    $jogo->setNome($nome)
+        ->setAnoLancamento($anoLancamento)
+        ->setempresa($empresa)
+        ->setpreco($preco);
+
+    if ($idCategoria) {
+        $categoria = new Categoria();
+        $categoria->setId($idCategoria);
+        $jogo->setCategoria($categoria);
+    }
+
+    if ($idPlataforma) {
+        $plataforma = new Plataforma();
+        $plataforma->setId($idPlataforma);
+        $jogo->setPlataforma($plataforma);
+    }
+
+    $jogoCont = new jogoController();
     $erros = $jogoCont->inserir($jogo);
 
-    
-    if (empty($erros)) {
+    if (!$erros) {
         header("location: listar.php");
         exit;
     }
-
 
     $msgErros = implode("<br>", $erros);
 }
